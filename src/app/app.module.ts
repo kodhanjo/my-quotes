a1d68f4e2728abaccf16b69ae70c545a66e621cb
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-17 18:02:19 +0300

    added appmodule.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesComponent } from './quotes-m/quotes.component';
 import { PlatesComponent } from './plates/plates.component';
 import { DetailsComponent } from './details/details.component';
 import { AddComponent } from './add/add.component';
@@ -28,5 +28,30 @@ import { DateDifferencePipe } from './pipe/date-difference.pipe';
   ],
   providers: [],
   bootstrap: [AppComponent]
+})import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { QuotesMainComponent } from './quotes-main/quotes-main.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesAddComponent } from './quotes-add/quotes-add.component';
+import { DateDifferencePipe } from './Pipes/DateDifference/date-difference.pipe';
+import { HighlighterDirective } from './Directives/Highlighter/highlighter.directive';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuotesMainComponent,
+    QuotesDetailsComponent,
+    QuotesAddComponent,
+    DateDifferencePipe,
+    HighlighterDirective,
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
+export class AppModule {}
+
 export class AppModule { }
