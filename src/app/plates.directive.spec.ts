8acdd661dcb97959ae6286f33f3c1eafa3714b33
kodhanjo <felix.osambo@student.moringaschool.com>
2021-07-17 16:18:02 +0300

    deleted autoated pipes

--- src/app/plates.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlatesDirective } from './plates.directive';
-
-describe('PlatesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PlatesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
